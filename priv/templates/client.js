faa41eb4949eabfec2eaec4510c41b73e82712da
Joseph Cosentino <jcosentino11@pm.me>
2024-08-01 21:05:36 -0700

    templated ws proto

---
+++ priv/templates/client.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const cellSize = 100;
+
+const startButton = document.getElementById("startButton");
+const playerScore = document.getElementById("playerScore");
+
+let board;
+let score = 0;
+
+const server = "{{ws_proto}}://" + window.location.host + "/server";
+const ws = new WebSocket(server);
+ws.onmessage = (event) => {
+  const gameState = JSON.parse(event.data);
+  updateState(gameState);
+  render();
+};
+
+const send = (message) => {
+  if (message && ws.readyState == WebSocket.OPEN) {
+    ws.send(message);
+  }
+};
+
+const updateState = (gameState) => {
+  board = gameState.player[0].board;
+  score = gameState.player[0].score;
+};
+
+const render = () => {
+  renderBoard(board);
+  renderScore();
+};
+
+const renderBoard = (board) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < board.length; i++) {
+    const x = (i % 5) * cellSize;
+    const y = Math.floor(i / 5) * cellSize;
+    ctx.fillStyle = board[i] === 1 ? "red" : "white";
+    ctx.fillRect(x, y, cellSize, cellSize);
+    ctx.strokeRect(x, y, cellSize, cellSize);
+  }
+};
+
+const renderScore = () => {
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "black";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText(`Score: ${score}`, 10, 10);
+};
+
+const handleMoleClick = (event) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+  const i = Math.floor(x / cellSize) + Math.floor(y / cellSize) * 5;
+  if (board[i] === 1) {
+    send(`hit${i + 1}`);
+  }
+};
+canvas.addEventListener("click", handleMoleClick);
+
+const handleStartButtonClick = () => {
+  send("ready");
+  startButton.disabled = true;
+};
+startButton.addEventListener("click", handleStartButtonClick);
---
+++ src/whackamole_template.erl
@@ -0,0 +1,45 @@
+-module(whackamole_template).
+
+-export([init/2]).
+
+template(Path) ->
+    case file:read_file(Path) of
+        {ok, Template} ->
+            bbmustache:render(Template, template_args());
+        Other ->
+            {error, Other}
+    end.
+
+template_args() ->
+    #{"ws_proto" => template_args_ws_proto()}.
+
+template_args_ws_proto() ->
+    case os:getenv("REBAR_PROFILE") of
+        prod -> "wss";
+        _ -> "ws"
+    end.
+
+content_type(Filepath) ->
+    case filename:extension(Filepath) of
+        <<".js">> -> <<"text/javascript">>;
+        <<".html">> -> <<"text/html">>;
+        _ -> <<"text/plain">>
+    end.
+
+init(Req, {priv_dir, Module, Path} = Opts) ->
+    case cowboy_req:path_info(Req) of
+        undefined ->
+            {ok, cowboy_req:reply(500, Req), error};
+        PathInfo ->
+            Filepath = filename:join([code:priv_dir(Module), Path] ++ PathInfo),
+            case template(Filepath) of
+                {error, _} ->
+                    {ok, cowboy_req:reply(500, Req), error};
+                Template ->
+                    ContentType = content_type(Filepath),
+                    Reply = cowboy_req:reply(
+                        200, #{<<"content-type">> => ContentType}, Template, Req
+                    ),
+                    {ok, Reply, Opts}
+            end
+    end.
