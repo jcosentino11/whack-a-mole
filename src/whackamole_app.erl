ef64834ad3f9261482e3c1792fe7659f0ae465f1
Joseph Cosentino <jcosentino11@pm.me>
2024-08-02 20:17:38 -0700

    add info endpoint

--- src/whackamole_app.erl
+++ src/whackamole_app.erl
@@ -12,6 +12,7 @@ start(_StartType, _StartArgs) ->
             {"/", cowboy_static, {priv_file, whackamole, "index.html"}},
             {"/static/[...]", whackamole_template, {priv_dir, whackamole, "templates"}},
             {"/icons/[...]", cowboy_static, {priv_dir, whackamole, "icons"}},
+            {"/info", whackamole_info_handler, []},
             {"/server", whackamole_server, []}
         ]}
     ]),
