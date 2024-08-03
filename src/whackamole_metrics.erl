95416e20dc9145848fd2c4c03fac92974ab9171e
Joseph Cosentino <jcosentino11@pm.me>
2024-08-02 18:38:28 -0700

    metrics module

--- src/whackamole_metrics.erl
+++ src/whackamole_metrics.erl
@@ -3,30 +3,42 @@
 -export([init/0]).
 
 -export([emit_ws_connected/0, emit_ws_disconnected/0]).
+-export([emit_game_started/0, emit_game_over/0]).
+
 -export([get_num_ws_connected/0]).
+-export([get_num_active_games/0]).
+
+-define(WEBSOCKET_CONN_COUNT, websocket_conn_count).
+-define(ACTIVE_GAME_COUNT, active_game_found).
 
 init() ->
     ets:new(metrics, [named_table, public, set]),
-    ets:insert(metrics, {websocket_conn_count, 0}),
-    ets:insert(metrics, {games_count, 0}).
+    ets:insert(metrics, {?WEBSOCKET_CONN_COUNT, 0}),
+    ets:insert(metrics, {?ACTIVE_GAME_COUNT, 0}).
 
 emit_ws_connected() ->
-    ets:update_counter(metrics, websocket_conn_count, {2, 1}).
+    inc_counter(?WEBSOCKET_CONN_COUNT).
 
 emit_ws_disconnected() ->
-    ets:update_counter(metrics, websocket_conn_count, {2, -1}).
+    dec_counter(?WEBSOCKET_CONN_COUNT).
 
 emit_game_started() ->
-    ets:update_counter(metrics, games_count, {2, 1}).
+    inc_counter(?ACTIVE_GAME_COUNT).
 
 emit_game_over() ->
-    ets:update_counter(metrics, games_count, {2, -1}).
+    dec_counter(?ACTIVE_GAME_COUNT).
 
 get_num_ws_connected() ->
-    get_counter(games_count).
+    get_counter(?WEBSOCKET_CONN_COUNT).
 
 get_num_active_games() ->
-    get_counter(games_count).
+    get_counter(?ACTIVE_GAME_COUNT).
+
+inc_counter(Name) -> 
+    ets:update_counter(metrics, Name, {2, 1}).
+
+dec_counter(Name) ->
+    ets:update_counter(metrics, Name, {2, -1}).
 
 get_counter(Name) ->
     case ets:lookup(metrics, Name) of
