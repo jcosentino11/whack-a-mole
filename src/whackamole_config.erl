3f2c80da3cbb192e0a73af15575b5d2597c82494
Joseph Cosentino <jcosentino11@pm.me>
2024-08-03 16:07:59 -0700

    fix env var integers

--- src/whackamole_config.erl
+++ src/whackamole_config.erl
@@ -14,20 +14,29 @@ app_env() ->
     os:getenv("APP_ENV", "dev").
 
 game_duration_millis() ->
-    os:getenv("GAME_DURATION_MILLIS", 10000).
+    getenv_int("GAME_DURATION_MILLIS", 10000).
 
 players_per_game() ->
-    os:getenv("PLAYERS_PER_GAME", 2).
+    getenv_int("PLAYERS_PER_GAME", 2).
 
 board_size() ->
-    os:getenv("BOARD_SIZE", 25).
+    getenv_int("BOARD_SIZE", 25).
 
 board_update_interval_millis() ->
-    os:getenv("BOARD_UPDATE_INTERVAL_MILLIS", 1000).
+    getenv_int("BOARD_UPDATE_INTERVAL_MILLIS", 1000).
 
 % effectively used to close the ws connection if not enough players are found for a game
 ws_idle_timeout_millis() ->
-    os:getenv("WS_IDLE_TIMEOUT_MILLIS", 5000).
+    getenv_int("WS_IDLE_TIMEOUT_MILLIS", 5000).
 
 ws_max_frame_size() ->
-    os:getenv("WS_MAX_FRAME_SIZE", (500 + players_per_game() * 2 * board_size())).
+    getenv_int("WS_MAX_FRAME_SIZE", (500 + players_per_game() * 2 * board_size())).
+
+getenv_int(Name, DefaultValue) ->
+    Val = os:getenv(Name, DefaultValue),
+    case string:to_integer(Val) of
+        {IntVal, _Rest} -> 
+            io:format("here", []),
+            IntVal;
+        _ -> DefaultValue
+    end.
